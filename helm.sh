28a31f32e5f0f40ce5d533c53063d3e46abc4375
jldeen <jedeen@outlook.com>
2019-07-17 11:04:17 -0700

    update demo commands

--- helm.sh
+++ helm.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# update these variables
+namespace=default
+helmReleaseName=jenkins
+
 # Setup Nginx Ingress controller
 h3 upgrade --install nginx-ingress stable/nginx-ingress
 
@@ -13,6 +17,7 @@ h3 repo add jetstack https://charts.jetstack.io
 
 #change namespace for temp broken --namespace flag
 #kubens and kubectx repo - https://github.com/ahmetb/kubectx | command: brew install kubectx
+# change namespace to kube-system
 kubens kube-system
 h3 upgrade --install cert-mgr jetstack/cert-manager
 
@@ -28,21 +33,25 @@ h3 version
 #helm3-alpha1 list releases
 h3 ls
 
-#quick change namespace
-kubens kube-system
+#helm3-alpha1 list all releases
+h3 ls --all-namespaces
 
-#quick change namespace
-kubens kubecon
-
-#quick change namespace
+#quick change namespace back to default (get out of kube-system)
 kubens default
 
 # Test configuration - Demo!
-h3 upgrade jenkins --install --namespace kubecon-live -f ./jenkins-values-demo.yaml stable/jenkins
+h3 upgrade jenkins --install --namespace $namespace -f ./jenkins-values-demo.yaml stable/jenkins
+
+# if using default namespace, the above will install to default regardless of which ns is specified with --namespace flag. --namespace flag currently is ignored, must change namespace first prior to helm chart install
+
+kubens $namespace
+
+# re-run helm 3 chart install
+
+h3 upgrade jenkins --install --namespace $namespace -f ./jenkins-values-demo.yaml stable/jenkins
+
+# get pods in specified namespace
+kubectl get pods -n $namespace
 
 #command to get admin password
-#update these parameters
-namespace=default
-helmReleaseName=jenkins
-
 printf $(kubectl get secret --namespace $namespace $helmReleaseName -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode);echo
