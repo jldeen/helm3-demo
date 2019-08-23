5b084d8c7077f58da5e62e51de6c319d2d28c8de
jldeen <jedeen@outlook.com>
2019-08-23 17:19:48 -0300

    Update helmVersion to alpha.2

--- scripts/setup-helm3.sh
+++ scripts/setup-helm3.sh
@@ -2,7 +2,7 @@
 set -eou pipefail
 
 #set helm version
-helmVersion=helm-v3.0.0-alpha.1-darwin-amd64.tar.gz
+helmVersion=helm-v3.0.0-alpha.2-linux-amd64.tar.gz
 
 #download helm version
 wget https://get.helm.sh/$helmVersion
