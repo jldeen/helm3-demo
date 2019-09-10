7b8e85a4b049cb3abf3bdc43c9559fa5e286848b
jldeen <jedeen@outlook.com>
2019-09-11 00:08:42 +0200

    helm3 beta updates

--- helm.sh
+++ helm.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 # update these variables
-namespace=default
+namespace=helmsummit
 helmReleaseName=jenkins
+
+# Setup Nginx Ingress controller - watch it fail
+h3 upgrade --install nginx-ingress stable/nginx-ingress
+
+#stable 
+
+# add helm repos
+helm repo add stable http://storage.googleapis.com/kubernetes-charts
+helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
 
 # Setup Nginx Ingress controller
 h3 upgrade --install nginx-ingress stable/nginx-ingress
@@ -15,11 +24,8 @@ kubectl apply \
 #add jetstack repo
 h3 repo add jetstack https://charts.jetstack.io
 
-#change namespace for temp broken --namespace flag
 #kubens and kubectx repo - https://github.com/ahmetb/kubectx | command: brew install kubectx
-# change namespace to kube-system
-kubens kube-system
-h3 upgrade --install cert-mgr jetstack/cert-manager
+h3 upgrade --install cert-mgr jetstack/cert-manager -n kube-system
 
 # Setup Certificate Cluster Issuer
 kubectl apply -f cluster-issuer.yaml
@@ -39,16 +45,23 @@ h3 ls --all-namespaces
 #quick change namespace back to default (get out of kube-system)
 kubens default
 
+# kubeval plugin insstall
+h3 plugin install https://github.com/instrumenta/helm-kubeval
+
+# check XDG cache path
+# linux:
+ls $HOME/.cache/helm
+#macOS:
+ls $HOME/Library/Caches/helm/
+
 # Test configuration - Demo!
 h3 upgrade jenkins --install --namespace $namespace -f ./jenkins-values-demo.yaml stable/jenkins
 
-# if using default namespace, the above will install to default regardless of which ns is specified with --namespace flag. --namespace flag currently is ignored, must change namespace first prior to helm chart install
+# h3 repo add
+h3 repo add jdcharts https://jdk8s.blob.core.windows.net/helm/
 
-kubens $namespace
-
-# re-run helm 3 chart install
-
-h3 upgrade jenkins --install --namespace $namespace -f ./jenkins-values-demo.yaml stable/jenkins
+# crochunter
+h3 upgrade crochunter --install --namespace default --set ingress.hostname=crochunter.h3.az.jessicadeen.com,image=jldeen/croc-hunter,imageTag=0.3.1 jdcharts/croc-hunter
 
 # get pods in specified namespace
 kubectl get pods -n $namespace
