2d25cb8070f9f84d78d5d69ad90c213f106e53e3
jldeen <jedeen@outlook.com>
2019-09-10 04:39:54 +0200

    update v3beta3

--- scripts/setup-helm3.sh
+++ scripts/setup-helm3.sh
@@ -2,14 +2,14 @@
 set -eou pipefail
 
 #set helm version
-helmVersion=helm-v3.0.0-alpha.2-linux-amd64.tar.gz
+helmVersion=helm-v3.0.0-beta.3-darwin-amd64.tar.gz
 
 #download helm version
 wget https://get.helm.sh/$helmVersion
 tar xvzf $helmVersion
 
 #mv helm3 binary to local tmp folder
-mkdir /usr/local/bin/tmp
+if [[ -e /usr/local/bin/tmp ]] ; then echo "tmp folder already exists" ; else mkdir /usr/local/bin/tmp ; fi
 mv darwin-amd64/helm /usr/local/bin/tmp/helm
 
 #TEMPORARY export $HELM_HOME so you don't overwrite Helm2 ~/.helm/
