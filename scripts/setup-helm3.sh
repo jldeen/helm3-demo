511d077a9240356b35e0bad037ef8f5767272ff1
Walter A. Boring IV <waboring@hemna.com>
2019-09-20 20:02:50 +0200

    Update setup-helm3.sh to work on linux
    Did a little bit of update on setup-helm3.sh to work on linux
    as well as macos.

--- scripts/setup-helm3.sh
+++ scripts/setup-helm3.sh
@@ -2,15 +2,28 @@
 set -eou pipefail
 
 #set helm version
-helmVersion=helm-v3.0.0-beta.3-darwin-amd64.tar.gz
+# MacOS
+#helmVersion=helm-v3.0.0-beta.3-darwin-amd64.tar.gz
+# Linux 64bit
+helmVersion=helm-v3.0.0-beta.3-linux-amd64.tar.gz
 
 #download helm version
 wget https://get.helm.sh/$helmVersion
 tar xvzf $helmVersion
 
 #mv helm3 binary to local tmp folder
-if [[ -e /usr/local/bin/tmp ]] ; then echo "tmp folder already exists" ; else mkdir /usr/local/bin/tmp ; fi
-mv darwin-amd64/helm /usr/local/bin/tmp/helm
+if [[ -e /usr/local/bin/tmp ]] ; then
+  echo "tmp folder already exists"
+else
+  sudo mkdir /usr/local/bin/tmp
+fi
+
+
+if [[ -e darwin-amd64 ]]; then
+  sudo mv darwin-amd64/helm /usr/local/bin/tmp/helm
+elif [[ -e linux-amd64 ]]; then
+  sudo mv linux-amd64/helm /usr/local/bin/tmp/helm
+fi
 
 #TEMPORARY export $HELM_HOME so you don't overwrite Helm2 ~/.helm/
 export HELM_HOME=/usr/local/bin/tmp/helm3
@@ -19,10 +32,18 @@ export HELM_HOME=/usr/local/bin/tmp/helm3
 h3=/usr/local/bin/tmp/helm
 
 #add alias to .zshrc or .bashrc
-echo "alias h3=/usr/local/bin/tmp/helm" >> ~/.zshrc
-# echo "alias h3=/usr/local/bin/tmp/helm" >> ~/.bashrc
+if [[ -e ~/.zshrc ]]; then
+  echo "alias h3=/usr/local/bin/tmp/helm" >> ~/.zshrc
+  source ~/.zshrc
+elif [[ -e ~/.bashrc ]]; then
+  echo "alias h3=/usr/local/bin/tmp/helm" >> ~/.bashrc
+  source ~/.bashrc
+fi
 
 # cleanup
 rm -rf $helmVersion
-rm -rf darwin-amd64
-
+if [[ -e darwin-amd64 ]]; then
+  rm -rf darwin-amd64
+elif [[ -e linux-amd64 ]]; then
+  rm -rf linux-amd64
+fi
